28adee21b680610b083b50f7cb54a4bf03b0b34d
Fahad Ahmed Chowdhury <fahadchy24@gmail.com>
2022-10-19 00:51:05 +0600

    project setup and breeze authentication added

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                'poppins': ['Poppins', 'sans-serif', ...defaultTheme.fontFamily.sans],
+            },
+            backgroundImage: {
+                'hero': 'url(../images/banner.png)',
+            },
+            container: {
+                padding: '2rem'
+            },
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
